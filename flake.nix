6b52d85d181f6c5cd04878154dc419e2dc7cbfc6
Shishir Srivastava <github@truemint.email>
2026-02-09 13:46:00 +0530

    Minor updates

--- flake.nix
+++ flake.nix
@@ -5,13 +5,16 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
-      inputs.nixpkgs.follows = "nixpkgs";		# Ensures that home-manager version is in sync with nixpkgs version
+      inputs.nixpkgs.follows = "nixpkgs";	# Ensures that packages used by home-manager come from the same nixpkgs version as that used by our flake
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }: {
-    nixosConfigurations.Truemint-NixOS = nixpkgs.lib.nixosSystem {
+  outputs = { nixpkgs, home-manager, ... } @ inputs: 
+    let
       system = "x86_64-linux";
+    in {
+    nixosConfigurations.Truemint-NixOS = nixpkgs.lib.nixosSystem {
+      specialArgs = { inherits inputs; };
       modules = [ 
         ./configuration.nix
         home-manager.nixosModules.home-manager
