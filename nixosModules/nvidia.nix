690868d52a73a3cc0961fbda2d85b6fe148264ad
Shishir Srivastava <github@truemint.email>
2026-02-17 23:12:37 +0530

    Refactors code

--- nixosModules/nvidia.nix
+++ nixosModules/nvidia.nix
@@ -5,16 +5,28 @@
   ...
 }: {
   nixpkgs.config.allowUnfree = true;
+
+  # The names of the video drivers the configuration supports.
   services.xserver.videoDrivers = ["nvidia"];
 
   hardware = {
+    # Whether to enable hardware accelerated graphics drivers
     graphics.enable = true;
 
     nvidia = {
+      # Whether to enable kernel modesetting when using the NVIDIA proprietary driver
       modesetting.enable = true;
+
+      # Whether to enable nvidia-settings, NVIDIA's GUI configuration tool.
       nvidiaSettings = true;
+
+      # Whether to enable experimental power management through systemd
       powerManagement.enable = false;
+
+      # The NVIDIA driver package to use
       package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+      # Whether to enable the open source NVIDIA kernel module
       open = true;
     };
   };
