b75fea04604905408750c12788f293d7446e841a
Shishir Srivastava <github@truemint.email>
2026-02-25 10:36:24 +0530

    Adds config for multi-gpu setup.

--- homeModules/hyprland.nix
+++ homeModules/hyprland.nix
@@ -180,7 +180,10 @@
 
     # Aquamarine env var to set an explicit list of GPUs to use
     # Reference: https://wiki.hypr.land/0.52.0/Configuring/Multi-GPU/
-    # AQ_DRM_DEVICES = "";
+    # Setting this to the AMD card path `/dev/dri/renderD129` ensures that Hyprland will use the AMD iGPU.
+    # Since we haven't included the Nvidia GPU path in this list, Hyprland will not touch the Nvidia dGPU at all.
+    AQ_DRM_DEVICES = "/dev/dri/renderD129";
+    # WLR_DRM_DEVICES = "/dev/dri/renderD129";	# Used by wlroots based compositors. Deprecated by Hyprland.
 
     # Helps Chromium/Electron apps prefer native Wayland instead of XWayland on NixOS.
     NIXOS_OZONE_WL = "1";
--- nixosModules/nvidia.nix
+++ nixosModules/nvidia.nix
@@ -1,4 +1,5 @@
-# Configuration specific to Nvidia GPU
+# Graphics config
+# Multi-GPU: https://wiki.hypr.land/0.52.0/Configuring/Multi-GPU/
 {
   config,
   pkgs,
@@ -7,39 +8,79 @@
   nixpkgs.config.allowUnfree = true;
 
   # The names of the video drivers the configuration supports.
-  services.xserver.videoDrivers = ["nvidia"];
+  services.xserver.videoDrivers = ["amdgpu" "nvidia"];
 
   hardware = {
-    # Whether to enable hardware accelerated graphics drivers
-    graphics.enable = true;
+    graphics = {
+      # Enable hardware accelerated graphics drivers
+      enable = true;
+
+      # Also install 32-bit drivers for 32-bit applications (such as Wine)
+      enable32bit = true;
+    };
+
+    amdgpu = {
+      # Enable loading amdgpu kernel module in stage 1.
+      # This helps with low resolution issues during early boot phases.
+      initrd.enable = true;
+    };
 
     nvidia = {
-      # Whether to enable kernel modesetting when using the NVIDIA proprietary driver
+      # Required by Wayland. Enable kernel modesetting when using the NVIDIA proprietary driver.
       modesetting.enable = true;
 
-      # Whether to enable nvidia-settings, NVIDIA's GUI configuration tool.
+      # Enable nvidia-settings, NVIDIA's GUI configuration tool.
       nvidiaSettings = true;
 
-      # Whether to enable experimental power management through systemd
-      powerManagement.enable = false;
+      # Enable experimental power management through systemd. Required for Wayland.
+      powerManagement = {
+        enable = true;
+        finegrained = true;
+      };
 
-      # The NVIDIA driver package to use
+      # Use the stable NVIDIA driver package
       package = config.boot.kernelPackages.nvidiaPackages.stable;
 
-      # Whether to enable the open source NVIDIA kernel module
-      open = true;
+      # Whether to enable the open source NVIDIA kernel module.
+      # Disabled since Nvidia cards work best with proprietary drivers.
+      open = false;
+
+      # Offload mode
+      prime = {
+        offload = {
+          enable = true;
+          enableOffloadCmd = true;
+        };
+
+        # Make sure you set correct Bus IDs for the system
+        # Use `lspci | grep VGA` to see list of display cards
+        # Current system shows AMD as 11:00.0
+        # We have set 17 since 11 (hex) equals 17 (decimal)
+        # Reference: https://wiki.nixos.org/wiki/NVIDIA
+        amdgpuBusId = "PCI:17:0:0";
+        nvidiaBusId = "PCI:1:0:0";
+      };
     };
   };
 
-  # Load nvidia drivers early to fix resolution issues with boot screens
-  boot = {
-    initrd.kernelModules = [
-      "nvidia"
-      "nvidia_modeset"
-      "nvidia_uvm"
-      "nvidia_drm"
-    ];
-
-    extraModulePackages = [config.boot.kernelPackages.nvidiaPackages.stable];
+  # Set of environment variables used in the global environment.
+  # We set relevant variables to force system to use AMD iGPU instead of Nvidia dGPU.
+  # We will also need to set Hyprland specific variables. See Hyprland config.
+  environment.sessionVariables = {
+    LIBVA_DRIVER_NAME = "radeonsi";
+    GBM_BACKEND = "radeonsi";
+    __GLX_VENDOR_LIBRARY_NAME = "mesa";
   };
+
+  # Load nvidia drivers early to fix resolution issues with boot screens
+  # boot = {
+  #  initrd.kernelModules = [
+  #    "nvidia"
+  #    "nvidia_modeset"
+  #    "nvidia_uvm"
+  #    "nvidia_drm"
+  #  ];
+
+  #  extraModulePackages = [config.boot.kernelPackages.nvidiaPackages.stable];
+  # };
 }
